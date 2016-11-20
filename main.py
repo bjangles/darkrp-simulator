cd3b744697f0a63a9dfc78500ef117c5e6adf6aa
bjangles <bojangles@airmail.cc>
2016-11-20 11:44:47 -0600

    added verb interpretation

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 #!/bin/python3
 
+import os
 import random
+import string
 
 newGameMessages = [
 "At last, you have finally finished downloading sixty gigabytes of content only paid users can access. Your eyes are filled with colors and your ears are overwhelmed with the sound of screaming prepubescent children. The world is your oyster.",
 "You have finally arrived in the promised land. It's a shame, though. You were just starting to like that Scary Monsters & Nice Sprites remix played at full volume.",
 "34 minutes and 12 seconds later. If only you had more time, you could've memorised the rules!",
+"Woah! You totally forgot you were joining a server. Now it's time to stop forgetting and start regretting.",
 ]
 
+misunderstoodMessages = [
+"I'm sorry, I didn't quite catch that.",
+"I'm not sure I understand what you mean.",
+"This is an English-only server.",
+"Huh?",
+"Pardon?",
+"Could you rephrase that?",
+]
+
+rules = [
+"=====Lasciate ogne speranza, voi ch'intrate=====",
+"Rule 1: No cheating/exploiting/hacking/misbehaving/tomfoolery/mischief/deviation",
+"Rule 2: No random death matching, henceforth known as 'artyem'",
+"Rule 3: Any actions, covert or otherwise, displayed in a roleplaying context must be advertised publicly to everybody on the server. Absolutely no exceptions.",
+"Rule 4: Absolutely no disrespect to members of our staff will be tolerated. Unprovoked abuse towards unprivileged players is otherwise acceptable.",
+"Rule 5: Artyem is absolutely prohibited, unless you provide a warning milliseconds beforehand.",
+"Rule 6: All roleplaying chat must be broadcast in out-of-character chat, in order to ensure maximum 'arpee' experience.",
+" *** Your eyes glaze over as the rule count extends into the hundreds. *** ",
+]
+
+#Verbs
+def read(args):
+    if len(args) < 2:
+        print("What do you want to read?")
+        print()
+        return
+    if args[1] == "rules":
+        for i in rules:
+            print(i)
+        print() 
+
+def respawn():
+    print("You respawn.")
+
+
 def parse(command):
-    print(command)
-    print("")
+    words = command.split(" ")
+    verb = words[0]
+    
+    if verb == "read":
+        read(command.split(" "))
+    elif verb == "respawn":
+        respawn()
+    elif True:
+        print(misunderstoodMessages[random.randint(1, len(misunderstoodMessages)-1)])
+        print()
 
 def newgame():
     #Global Variables
@@ -42,8 +88,12 @@ def newgame():
     hasShotgun = False
     hasM4A1 = False
     hasDEagle = False
+
+    for i in range(1, 60):
+        print()
     
     print(newGameMessages[random.randint(1, len(newGameMessages))-1])
+    print()
 
     listen()
 
